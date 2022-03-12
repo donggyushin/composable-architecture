e3d8a41a0955e8ee8a80bcaa510739cf5348e319
신동규 <donggyu9410@gmail.com>
2022-03-13 01:34:43 +0900

    feat: 스토리보드 삭제

--- composable-architecture/composable-architecture/ViewController.swift
+++ composable-architecture/composable-architecture/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        configUI()
+    }
+    
+    private func configUI() {
+        
+    }
 }
 
