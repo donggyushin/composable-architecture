f8ff8c818d4d3be6a822fd841dc2e9c4c972254f
신동규 <donggyu9410@gmail.com>
2022-03-13 01:39:55 +0900

    feat: ui 추가

--- composable-architecture/composable-architecture/ViewController.swift
+++ composable-architecture/composable-architecture/ViewController.swift
@@ -9,6 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let funFactLabel: UILabel = {
+        let view = UILabel()
+        view.text = "Fun fact"
+        view.textAlignment = .center
+        return view
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let view = UIButton(configuration: .tinted(), primaryAction: .init(handler: { _ in
+            print("DEBUG: plus button")
+        }))
+        view.setTitle("+", for: .normal)
+        return view
+    }()
+    
+    private lazy var minusButton: UIButton = {
+        let view = UIButton(configuration: .tinted(), primaryAction: .init(handler: { _ in
+            print("DEBUG: minus button")
+        }))
+        view.setTitle("-", for: .normal)
+        return view
+    }()
+    
+    private let countLabel: UILabel = {
+        let view = UILabel()
+        view.text = "0"
+        return view
+    }()
+    
+    private lazy var horizontalStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [plusButton, countLabel, minusButton])
+        view.axis = .horizontal
+        view.spacing = 8
+        return view
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [funFactLabel, horizontalStackView])
+        view.axis = .vertical
+        view.spacing = 20
+        return view
+    }()
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
@@ -22,7 +65,14 @@ class ViewController: UIViewController {
     }
     
     private func configUI() {
+        view.addSubview(verticalStackView)
         
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            verticalStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            verticalStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
 
